1f6432b64ad6552e9adc71c11c036bb5dab9e4f5
china-feng <31477408+china-feng@users.noreply.github.com>
2018-05-10 14:21:24 +0700

    180510
    180510

---
+++ index.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/10
+ * Time: 13:17
+ */
+class Bim
+{
+    public function __construct($id=1){
+
+    }
+
+    public function doSomething()
+    {
+        echo __METHOD__, '|';
+    }
+}
+
+class Bar
+{
+    private $bim;
+
+    public function __construct(Bim $bim)
+    {
+        $this->bim = $bim;
+    }
+
+    public function doSomething()
+    {
+        $this->bim->doSomething();
+        echo __METHOD__, '|';
+    }
+}
+
+class Foo
+{
+    private $bar;
+
+    public function __construct(Bar $bar)
+    {
+        $this->bar = $bar;
+    }
+
+    public function doSomething()
+    {
+        $this->bar->doSomething();
+        echo __METHOD__;
+    }
+}
+
+class Container
+{
+    private $s = array();
+
+    public function __set($k, $c)
+    {
+        $this->s[$k] = $c;
+    }
+
+    public function __get($k)
+    {
+        // return $this->s[$k]($this);
+        return $this->build($this->s[$k]);
+    }
+
+    /**
+     * 自动绑定（Autowiring）自动解析（Automatic Resolution）
+     *
+     * @param string $className
+     * @return object
+     * @throws Exception
+     */
+    public function build($className)
+    {
+        // 如果是匿名函数（Anonymous functions），也叫闭包函数（closures）
+        //instanceof 检查某个变量是否属于某个某个类的实例
+        if ($className instanceof Closure) {
+            // 执行闭包函数，并将结果
+            return $className($this);
+        }
+
+        /** @var ReflectionClass $reflector */
+        $reflector = new ReflectionClass($className);
+
+        // 检查类是否可实例化, 排除抽象类abstract和对象接口interface
+        if (!$reflector->isInstantiable()) {
+            throw new Exception("Can't instantiate this.");
+        }
+
+        /** @var ReflectionMethod $constructor 获取类的构造函数 */
+        $constructor = $reflector->getConstructor();
+
+        // 若无构造函数，直接实例化并返回
+        if (is_null($constructor)) {
+            return new $className;
+        }
+
+        // 取构造函数参数,通过 ReflectionParameter 数组返回参数列表
+        $parameters = $constructor->getParameters();
+        if (!$parameters) {
+            return new $className;
+        }
+        // 递归解析构造函数的参数
+        $dependencies = $this->getDependencies($parameters);
+//        var_dump($dependencies);exit;
+        // 创建一个类的新实例，给出的参数将传递到类的构造函数。
+        var_dump($dependencies);
+        return $reflector->newInstanceArgs($dependencies);  //可以理解以$dependencies参数创建$reflector类
+    }
+
+    /**
+     * @param array $parameters
+     * @return array
+     * @throws Exception
+     */
+    public function getDependencies($parameters)
+    {
+        $dependencies = [];
+
+        /** @var ReflectionParameter $parameter */
+        foreach ($parameters as $parameter) {
+            /** @var ReflectionClass $dependency */
+            $dependency = $parameter->getClass(); //返回对象实例所属类的名字。 如果$parameter不是对象  则返回null
+//            var_dump($dependency);
+            if (is_null($dependency)) {
+                // 是变量,有默认值则设置默认值
+                $dependencies[] = $this->resolveNonClass($parameter);
+            } else {
+                // 是一个类，递归解析
+                $dependencies[] = $this->build($dependency->name);
+            }
+        }
+
+        return $dependencies;
+    }
+
+    /**
+     * @param ReflectionParameter $parameter
+     * @return mixed
+     * @throws Exception
+     */
+    public function resolveNonClass($parameter)
+    {
+        // 有默认值则返回默认值
+        if ($parameter->isDefaultValueAvailable()) {
+//            var_dump($parameter->getDefaultValue());
+            return $parameter->getDefaultValue();
+        }
+
+        throw new Exception('I have no idea what to do here.');
+    }
+}
+
+// ----
+//$c = new Container();
+//$c->bar = 'Bar';
+//$c->foo = function ($c) {
+//    return new Foo($c->bar);
+//};
+//// 从容器中取得Foo
+//$foo = $c->foo;
+//$foo->doSomething(); // Bim::doSomething|Bar::doSomething|Foo::doSomething
+
+// ----
+$di = new Container();
+
+$di->foo = 'Foo';
+
+/** @var Foo $foo */
+$foo = $di->foo;
+
+var_dump($foo);
+/*
+Foo#10 (1) {
+  private $bar =>
+  class Bar#14 (1) {
+    private $bim =>
+    class Bim#16 (0) {
+    }
+  }
+}
+*/
+
+$foo->doSomething(); // Bim::doSomething|Bar::doSomething|Foo::doSomething
